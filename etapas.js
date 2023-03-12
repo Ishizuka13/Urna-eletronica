92e8626852252df8e3bf92ded93d07561345b7fe
Miguel <miguelishizuka4@gmail.com>
2023-03-12 17:53:56 -0300

    .

---
+++ etapas.js
@@ -0,0 +1,40 @@
+let etapas = [
+    {
+        titulo:'VEREADOR',
+        numeros:5,
+        candidatos: [
+            {
+                numero:'38111',
+                name: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {url:'mulher1.png', legenda:'Vereadora', small:false}
+                ]
+            },
+            {
+                numero:'77222',
+                name:'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'mulher2.png', legenda: 'Vereadora'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros:2,
+        candidatos: [
+            {
+                numero:'99',
+                name:'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos:[
+                    {url:'homem1.png', legenda:'Prefeito'},
+                    {url:'homem2.png', legenda:'Vice-Prefeito', small:true}
+                ]
+            },
+        ]
+    }
+];
